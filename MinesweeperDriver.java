d53d455a888bc1552d7bcda3a397901c23352c7f
Richard Wu <wur3@tcnj.edu>
2017-01-13 12:05:19 -0500

    Added driver class

---
+++ MinesweeperDriver.java
@@ -0,0 +1,15 @@
+package labs;
+
+import java.util.Scanner;
+
+public class MinesweeperDriver {
+	public static void main(String[] args) {		
+			
+		Minesweeper.makeMinefield();
+		
+		Minesweeper.makeMines();
+				
+		Minesweeper.printMinefield();
+	
+	}
+}
