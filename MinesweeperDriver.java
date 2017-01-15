4f14eb791aa0f342e1b54ca6413d725692e1795e
Richard Wu <wur3@tcnj.edu>
2017-01-14 22:09:26 -0500

    Now tests guess()

--- MinesweeperDriver.java
+++ MinesweeperDriver.java
@@ -4,11 +4,14 @@ import java.util.Scanner;
 
 public class MinesweeperDriver {
 	public static void main(String[] args) {		
-			
+		//set up	
 		Minesweeper.makeMinefield();
-		
 		Minesweeper.makeMines();
-				
+
+		Minesweeper.printMinefield();
+		
+		//user starts guessing
+		Minesweeper.guess();
 		Minesweeper.printMinefield();
 	
 	}
